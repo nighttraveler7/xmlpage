a92200087fdf6234ad3325020a49b8dfabd87d89
廣田雄一 <nighttraveler7@gmail.com>
2019-11-12 20:48:40 +0900

    rename some files

---
+++ src/Template.php
@@ -0,0 +1,46 @@
+<?php
+namespace nighttraveler7\XMLPage;
+
+class Template {
+	public $page_name;
+	public $template_path;
+	public $raw_data;
+	public $parsed_data;
+
+	public static function vksprintf($str, $args) {
+		if (is_object($args)) {
+			$args = get_object_vars($args);
+		}
+		$map = array_flip(array_keys($args));
+		$new_str = preg_replace_callback('/(^|[^%])%([a-zA-Z0-9_-]+)\$/', function ($m) use ($map) {
+			return $m[1] . '%' . ($map[$m[2]] + 1) . '$';
+		}, $str);
+		return vsprintf($new_str, $args);
+	}
+
+	public function __construct($page_name = NULL, $template_path = NULL) {
+		if (is_null($page_name)) {
+			$page_name = XMLPage::get_current_page_name();
+		}
+		if (is_null($template_path)) {
+			$template_path = $_SERVER['DOCUMENT_ROOT'] . '/template.php';
+		}
+		$this->page_name = $page_name;
+		$this->template_path = $template_path;
+	}
+
+	public function load_template($args = array()) {
+		$page_name = $this->page_name;
+		ob_start();
+		require_once($this->template_path);
+		$this->raw_data = ob_get_contents();
+		ob_end_clean();
+		
+		$this->parsed_data = self::vksprintf($this->raw_data, $args);
+	}
+
+	public function show_template() {
+		echo $this->parsed_data;
+	}
+}
+?>
---
+++ src/XMLPage.php
@@ -0,0 +1,25 @@
+<?php
+namespace nighttraveler7\XMLPage;
+
+class XMLPage {
+	const XMLPAGES_PATH = '/xmlpages';
+
+	public $page_name = "404";
+	public $xmlpages_path;
+	protected $file_name;
+	public $text_content;
+	public $xml_content;
+
+	public static function get_current_page_name() {
+		return basename($_SERVER['SCRIPT_NAME'], '.php');
+	}
+
+	public function __construct($page_name) {
+		$this->xmlpages_path = $_SERVER['DOCUMENT_ROOT'] . self::XMLPAGES_PATH;
+		$this->page_name = $page_name;
+		$this->file_name = $this->xmlpages_path . '/' . $page_name . '.xml';
+		$this->text_content = file_get_contents($this->file_name);
+		$this->xml_content = new \SimpleXMLElement($this->text_content);
+	}
+}
+?>
